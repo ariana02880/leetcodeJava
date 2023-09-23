fd8b26db0585985907e636a566d87c7358524443
Ariana Li <arianali@Arianas-MBP.lan>
2023-09-23 15:20:18 -0700

    oa aws

---
+++ src/realProblem/aws1.java
@@ -0,0 +1,37 @@
+package realProblem;
+// https://leetcode.com/discuss/interview-experience/1379020/google-phone-screen
+
+import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.Map;
+import java.util.Queue;
+import java.util.Stack;
+
+class Solution {
+  public int countGroups(int[] arr) {
+    int n = arr.length;
+    Stack<Integer> stk = new Stack<>();
+    int cnt = 0;
+    for (int r = 0; r <= n; r++) {
+      int cur = (r == n) ? Integer.MAX_VALUE : arr[r];
+      while (!stk.isEmpty() && arr[stk.peek()] < cur) {
+        int j = stk.pop();
+        int l = stk.isEmpty() ? -1 : stk.peek();
+        cnt += (r - j) + (j - l) - 1;
+      }
+      stk.push(r);
+    }
+    return cnt;
+  }
+}
+
+public class aws1 {
+  public static void main(String arg[]) {
+
+    Solution s = new Solution();
+    int[] arr = { 100, 1, 5, 1, 100 };
+    int a = s.countGroups(arr);
+
+    System.out.println("Hello World");
+  }
+}
--- src/realProblem/tiktok2.java
+++ src/realProblem/tiktok2.java
@@ -1,91 +1,91 @@
-package realProblem;
-// https://leetcode.com/discuss/interview-experience/1379020/google-phone-screen
+// package realProblem;
+// // https://leetcode.com/discuss/interview-experience/1379020/google-phone-screen
 
-import java.util.HashMap;
-import java.util.LinkedList;
-import java.util.Map;
-import java.util.Queue;
+// import java.util.HashMap;
+// import java.util.LinkedList;
+// import java.util.Map;
+// import java.util.Queue;
 
-/*
- * const input = [
-    "app/index.html",
-    "app/README.md",
-    "app/src/App.jsx",
-    "app/src/App.css"
-]
-
-
-
-function generateFileTree (input) {
-    // write your code here
-}
-
-generateFileTree(input)
+// /*
+//  * const input = [
+//     "app/index.html",
+//     "app/README.md",
+//     "app/src/App.jsx",
+//     "app/src/App.css"
+// ]
 
 
-output
-// --app
-// --index.html
-// --README.md
-// --src
-//   --App.jsx
-//   --App.css
 
- */
+// function generateFileTree (input) {
+//     // write your code here
+// }
 
-class TrieNode {
-    boolean isFile = false;
-    Map<String, TrieNode> next;
+// generateFileTree(input)
 
-    TrieNode() {
-        next = new HashMap<>();
-    }
-}
 
-class Solution {
-    TrieNode root = new TrieNode();
-    public void generateFileTree(String[] input) {
-        for (String s : input) {
-            insertWord(s);
-        }
-        print(root, "");
+// output
+// // --app
+// // --index.html
+// // --README.md
+// // --src
+// //   --App.jsx
+// //   --App.css
 
-    }
+//  */
 
-    private void print(TrieNode n, String spaces) {
-        n.next.forEach((key, value) -> {
-            System.out.println(spaces + key);
-            print(value, spaces + " ");
-        });
-    }
+// class TrieNode {
+//     boolean isFile = false;
+//     Map<String, TrieNode> next;
 
-    public void insertWord(String input) {
-        TrieNode cur = root;
-        String[] inputArr = input.split("/");
-        for (int i = 0; i < inputArr.length; i++) {
-            cur.next.putIfAbsent(inputArr[i], new TrieNode());
-            
-            if (i == inputArr.length - 1) {
-                cur.next.get(inputArr[i]).isFile = true;
-            }
-            cur = cur.next.get(inputArr[i]);
-        }
+//     TrieNode() {
+//         next = new HashMap<>();
+//     }
+// }
 
-    }
-}
+// class Solution {
+//     TrieNode root = new TrieNode();
+//     public void generateFileTree(String[] input) {
+//         for (String s : input) {
+//             insertWord(s);
+//         }
+//         print(root, "");
 
-public class tiktok2 {
-    public static void main(String arg[]) {
+//     }
 
-        Solution s = new Solution();
-        String[] input = {
-                "app/index.html",
-                "app/README.md",
-                "app/src/App.jsx",
-                "app/src/App.css"
-        };
-        s.generateFileTree(input);
+//     private void print(TrieNode n, String spaces) {
+//         n.next.forEach((key, value) -> {
+//             System.out.println(spaces + key);
+//             print(value, spaces + " ");
+//         });
+//     }
 
-        System.out.println("Hello World");
-    }
-}
+//     public void insertWord(String input) {
+//         TrieNode cur = root;
+//         String[] inputArr = input.split("/");
+//         for (int i = 0; i < inputArr.length; i++) {
+//             cur.next.putIfAbsent(inputArr[i], new TrieNode());
+
+//             if (i == inputArr.length - 1) {
+//                 cur.next.get(inputArr[i]).isFile = true;
+//             }
+//             cur = cur.next.get(inputArr[i]);
+//         }
+
+//     }
+// }
+
+// public class tiktok2 {
+//     public static void main(String arg[]) {
+
+//         Solution s = new Solution();
+//         String[] input = {
+//                 "app/index.html",
+//                 "app/README.md",
+//                 "app/src/App.jsx",
+//                 "app/src/App.css"
+//         };
+//         s.generateFileTree(input);
+
+//         System.out.println("Hello World");
+//     }
+// }
